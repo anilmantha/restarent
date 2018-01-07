87f789c293ab6629e8c6c4684565260e5dad68a0
abc <anil.mdeveloper@gmail.com>
2018-01-07 22:33:35 +0530

    added my github name

--- restarent/restarent/src/main/java/com/restarent/controller/RestarentOpeningPage.java
+++ restarent/restarent/src/main/java/com/restarent/controller/RestarentOpeningPage.java
@@ -1,9 +1,22 @@
 package com.restarent.controller;
 
-@controller
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ControllerAdvice
 public class RestarentOpeningPage {
 	
+	
+	@RequestMapping("/openRestarentPage")
 	public String openRestarentPage() {
+		
+		
+		return "restarent";
+		
+	}
+	
+	@RequestMapping("/saveRestarentData")
+	public String saveRestarentPage() {
 		
 		
 		return "restarent";
